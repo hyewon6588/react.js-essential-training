54dae556dbeebf75e552b1e250f0ee43a092f761
hyewon6588 <hyewon6588@naver.com>
2024-06-03 03:42:07 +0900

    added useEffect hook with function and dependency array

--- Exercise Files/Ch04/04_05/start/react-app/src/App.js
+++ Exercise Files/Ch04/04_05/start/react-app/src/App.js
@@ -4,6 +4,10 @@ import { useState,useEffect } from "react";
 function App() {
   const [emotion, setEmotion] = useState("happy");
 
+  useEffect(()=>{
+    console.log(`It's ${emotion} right now`);
+  },[emotion]);
+
   return (
     <div className="App">
       <h1>Current emotion is {emotion}</h1>
