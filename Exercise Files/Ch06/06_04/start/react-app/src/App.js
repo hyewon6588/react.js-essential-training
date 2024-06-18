370b55e8766b3796192a38aa3e75dfd1674057ea
hyewon6588 <hyewon6588@naver.com>
2024-06-19 04:10:38 +0900

    changed endpoint of API, data. added query and request option to fetch data with graphQL

--- Exercise Files/Ch06/06_04/start/react-app/src/App.js
+++ Exercise Files/Ch06/06_04/start/react-app/src/App.js
@@ -1,12 +1,27 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 
-function GithubUser({ name, location, avatar }) {
+const query=`
+query{
+  allLifts{
+    name
+    elevationGain
+    status
+  }
+}
+`;
+
+const opts={
+  method:"POST",
+  headers:{"Content-Type":"application/json"},
+  body:JSON.stringify({query})
+}
+
+function Lift({ name, elevationGain,status }) {
   return (
     <div>
       <h1>{name}</h1>
-      <p>{location}</p>
-      <img src={avatar} height={150} alt={name} />
+      <p>{elevationGain} {status}</p>
     </div>
   );
 }
@@ -15,11 +30,12 @@ function App() {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
-
+  console.log(data,"DATA!!!");
   useEffect(() => {
     setLoading(true);
     fetch(
-      `https://api.github.com/users/moonhighway`
+      `https://snowtooth.moonhighway.com/`,
+      opts
     )
       .then((response) => response.json())
       .then(setData)
@@ -32,11 +48,15 @@ function App() {
     return <pre>{JSON.stringify(error)}</pre>;
   if (!data) return null;
   return (
-    <GithubUser
-      name={data.name}
-      location={data.location}
-      avatar={data.avatar_url}
-    />
+    <div>
+      {data.data.allLifts.map(lift=>(
+      <Lift 
+        name={lift.name} 
+        elevationGain={lift.elevationGain} 
+        status={lift.status}
+      />
+      ))}
+    </div>
   );
 }
 
